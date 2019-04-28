bf94c0aa3f6c0e63d911d001bf8232ce3b7a8230
Leo Martinusic <lmartinusic@leos-mbp.home>
2019-04-28 13:36:08 +0200

    Loadscene Intro to Main Scene

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+	public void LoadSceneMain () {
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	public void LoadSceneEnd () {
+		SceneManager.LoadScene ("EndScene");
+	}
+}
